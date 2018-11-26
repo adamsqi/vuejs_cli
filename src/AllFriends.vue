db5c5f841c03d7cc193340b28a4126a4231102e3
dams822 <dams822@gmail.com>
2018-11-26 23:21:12 +0100

    creation of some vue templates

---
+++ src/AllFriends.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="all-friends">
+        <h2>All Friends</h2>
+        <div v-for="(friend, index) in friends" :key="index">
+            <span>{{ friend.name }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AllFriends',
+    data () {
+        return {
+            friends: [
+                { name: 'Mario', online: true},
+                { name: 'Adam', online: true},
+                { name: 'Henry', online: true},
+                { name: 'John', online: true}
+            ]
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/OnlineFriends.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="online-friends">
+        <h2>Online Friends</h2>
+        <div v-for="(friend, index) in friends" :key="index">
+            <span v-if="friend.online">{{ friend.name }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OnlineFriends',
+    data () {
+        return {
+            friends: [
+                { name: 'Mario', online: true},
+                { name: 'Adam', online: false},
+                { name: 'Henry', online: true},
+                { name: 'John', online: false}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
